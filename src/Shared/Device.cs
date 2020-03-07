d9469feaa9b058ebb436055bd64cf46d7dac8f44
shibatea <ggl.shibata@gmail.com>
2020-03-07 17:45:06 +0900

    クライアントサイドとサーバーサイドから参照されるモデルクラスを追加

---
+++ src/Shared/Device.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace BlazorWOL.Shared
+{
+    public class Device
+    {
+        public Guid Id { get; set; } = Guid.NewGuid();
+        public string Name { get; set; }
+        public string MACAddress { get; set; }
+    }
+}
